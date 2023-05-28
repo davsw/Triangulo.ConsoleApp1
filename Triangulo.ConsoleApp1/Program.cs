cba60744f13e4ec615560a2f014d250053131bf2
davi <davi@DESKTOP-P8830AD>
2023-05-28 20:58:05 -0300

    Add project files.

---
+++ Triangulo.ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo.ConsoleApp1
+
+{
+    class TesteTriangulo
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo1 = new Triangulo(10, 10, 10);
+
+            string tipoTriangulo1 = triangulo1.ObterTipo();
+
+            Console.WriteLine(tipoTriangulo1); //Eqüilátero
+
+            Triangulo triangulo2 = new Triangulo(12, 10, 10);
+
+            string tipoTriangulo2 = triangulo2.ObterTipo();
+
+            Console.WriteLine(tipoTriangulo2); //Isósceles
+
+            Triangulo triangulo3 = new Triangulo(12, 14, 16);
+
+            string tipoTriangulo3 = triangulo3.ObterTipo();
+
+            Console.WriteLine(tipoTriangulo3); //Escaleno
+
+            Console.WriteLine("O numero de triangulos criados foram: " /*+ Triangulo.quantidadeTriangulos*/);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Triangulo.ConsoleApp1/triangulo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo.ConsoleApp1
+{
+    public class Triangulo
+    {
+        private int ladoX;
+        private int ladoY;
+        private int ladoZ;
+
+        public string Tipo;
+
+        private static int quantidadeTriangulos = 0;
+
+        public Triangulo(int ladoX, int ladoY, int ladoZ)
+        {
+            
+            this.ladoX = ladoX;
+            this.ladoY = ladoY;
+            this.ladoZ = ladoZ;
+
+            quantidadeTriangulos++;
+        }
+
+        public string ObterTipo()
+        {
+            string tipo;
+
+           
+            if (LadoXInvalido() || LadoYInvalido() || LadoZInvalido())
+                tipo = "Invalido";
+
+            else if (ladoX == ladoY && ladoY == ladoZ)
+                tipo = "Eqüilátero";
+
+            else if (ladoX != ladoY && ladoY != ladoZ && ladoZ != ladoX)
+                tipo = "Escaleno";
+
+            else
+                tipo = "Isósceles";
+
+            return tipo;
+        }
+
+        private bool LadoZInvalido()
+        {
+            bool ladoZInvalido = ladoZ > ladoX + ladoY;
+
+            return ladoZInvalido;
+        }
+
+        private bool LadoYInvalido()
+        {
+
+            bool ladoXInvalido = ladoX > ladoZ + ladoY;
+
+            return ladoXInvalido;
+        }
+
+        private bool LadoXInvalido()
+        {
+
+            bool ladoYInvalido = ladoY > ladoZ + ladoX;
+
+            return ladoYInvalido;
+        }
+    }
+}
